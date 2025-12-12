e53196716a4173a6ee57f66ed584e948734c5c7e
mygit021 <103528857+mygit021@users.noreply.github.com>
2025-12-12 18:49:24 +0530

    added commented description

--- milestone.sql
+++ milestone.sql
@@ -1,3 +1,4 @@
+-- b) Customers who changed policy type (current & previous)
 WITH ordered AS (
           SELECT f.Customer_Id, f.Policy_Id, p.Policy_Type, f.Policy_Start_Dt,
                  ROW_NUMBER() OVER (PARTITION BY f.Customer_Id ORDER BY f.Policy_Start_Dt DESC) rn
@@ -13,35 +14,36 @@ WITH ordered AS (
         WHERE cur.rn = 1 AND prev.Policy_Type IS NOT NULL AND cur.Policy_Type <> prev.Policy_Type;
 
 
-
+-- c) Total policy amount by all customers and regions
 SELECT Customer_Id, SUM(Total_Policy_Amt) AS Total_Policy_Amt
         FROM fact_policy_txn
         GROUP BY Customer_Id;
 
-
+--d) Total policy amount for policy type = 'Auto'
 SELECT SUM(f.Total_Policy_Amt) AS total_auto
         FROM fact_policy_txn f
         LEFT JOIN dim_policy p ON f.Policy_Id = p.Policy_Id
         WHERE p.Policy_Type = 'Auto';
 
-
+-- e) Total policy amount: East+West & Quarterly in 2012
 SELECT SUM(Total_Policy_Amt) AS total_east_west_quarterly_2012
         FROM fact_policy_txn
         WHERE Region IN ('East','West')
           AND Policy_Term = 'Quarterly'
           AND substr(Policy_Start_Dt,1,4) = '2012';
 
-
+-- f) Customers whose marital status changed
 SELECT Customer_Id
         FROM dim_customer
         GROUP BY Customer_Id
         HAVING COUNT(DISTINCT Maritial_Status) > 1;
 
-
+-- g) Display all regions' customer + policy + address + policy details
 SELECT f.*, p.Policy_Name, p.Policy_Type, c.Customer_Name, c.Country, c.State, c.City, c.Postal_Code
         FROM fact_policy_txn f
         LEFT JOIN dim_policy p ON f.Policy_Id = p.Policy_Id
         LEFT JOIN dim_customer c ON f.Customer_Id = c.Customer_Id;
+
 
 
 
